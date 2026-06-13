f1490bfc2e23fc2784759503dbe56a6cd5c98fdb
Vikram Maurya <vikram@example.com>
2026-06-14 05:19:02 +0530

    Initial commit after cleanup

---
+++ README.md
@@ -0,0 +1,9 @@
+# 🎬 Movie Recommender App
+
+This is a Streamlit-based web app that recommends movies similar to the one you select.  
+It uses a precomputed similarity matrix and movie dictionary stored in `.pkl` files.
+
+## 🚀 How to Run Locally
+1. Install dependencies:
+   ```bash
+   pip install -r requirements.txt
---
+++ app.py
@@ -0,0 +1,76 @@
+import streamlit as st
+import pickle
+import pandas as pd
+import requests
+
+
+def fetch_p(title):
+    api_key = "6391fe0c"  # <--- PASTE YOUR KEY HERE
+    # We are just using title here since extracting year from your pickle might be tricky
+    url = f"http://www.omdbapi.com/?t={title}&apikey={api_key}"
+
+    try:
+        response = requests.get(url)
+        data = response.json()
+        if data.get('Poster') and data.get('Poster') != 'N/A':
+            return data['Poster']
+    except Exception as e:
+        print(e)
+
+    # Fallback image if OMDb fails or doesn't have the poster
+    return "https://via.placeholder.com/500x750?text=No+Poster"
+
+
+
+
+
+def recommend(movie):
+    index = movies[movies['title'] == movie].index[0]
+    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+    recommended_movies =[]
+    recommended_movies_posters = []
+    for i in distances[1:6]:
+        movie_title = movies.iloc[i[0]].title
+        recommended_movies.append(movie_title)
+        recommended_movies_posters.append(fetch_p(movie_title))
+    return recommended_movies, recommended_movies_posters
+
+
+
+
+movie_dict =pickle.load(open('movie_dict.pkl','rb'))
+movies=pd.DataFrame(movie_dict)
+similarity =pickle.load(open('similarity.pkl','rb'))
+
+st.title("Movie Recommendation System")
+
+selected_movie_name = st.selectbox(
+    "ENTER A MOVIE HERE",
+    movies['title'].values,
+)
+st.write("You selected:", selected_movie_name)
+
+
+
+
+if st.button("Recommend"):
+    names, posters = recommend(selected_movie_name)
+
+    # Create 5 columns to display movies side-by-side
+    col1, col2, col3, col4, col5 = st.columns(5)
+
+    with col1:
+        st.text(names[0])
+        st.image(posters[0])
+    with col2:
+        st.text(names[1])
+        st.image(posters[1])
+    with col3:
+        st.text(names[2])
+        st.image(posters[2])
+    with col4:
+        st.text(names[3])
+        st.image(posters[3])
+    with col5:
+        st.text(names[4])
+        st.image(posters[4])
